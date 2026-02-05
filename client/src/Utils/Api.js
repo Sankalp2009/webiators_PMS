0dd2e3d276cecc58458260a8a34b0cad821af09e
Sankalp Patel <101709238+Sankalp2009@users.noreply.github.com>
2026-02-05 19:37:29 +0530

    Fix_CORS

--- client/src/Utils/Api.js
+++ client/src/Utils/Api.js
@@ -6,6 +6,7 @@ const API_BASE_URL = "https://webiators-pms.onrender.com/api/v1";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
+    'Access-Control-Allow-Origin': '*',
     "Content-Type": "application/json",
   },
   withCredentials: true,
